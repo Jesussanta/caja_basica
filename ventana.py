3f9a887cde0327cefb74900cfe898b505b73a949
Jesussanta <jsanta@umariana.edu.co>
2021-08-06 11:32:29 -0500

    1.1

--- ventana.py
+++ ventana.py
@@ -11,7 +11,7 @@ class Ventana(Frame):
     cont = 0   
     def __init__(self, master=None):
         
-        super().__init__(master,width=700, height=260)
+        super().__init__(master,width=1280, height=720)
         self.master = master
         self.pack()
         self.create_widgets()
@@ -24,30 +24,30 @@ class Ventana(Frame):
             self.grid.insert("",END, text= row[0], values=(row[1],row[2],row[3]))
     def habilib(self,bn):
         if bn == 1:
-            self.btnAdd.configure(bg="#03738C",fg="white")
-            self.btnDelet.configure(bg="#013440",fg="white")
-            self.btnChan.configure(bg="#013440",fg="white")
-            self.btnShow.configure(bg="#013440",fg="white")
+            self.btnAdd.configure(bg="#263238",fg="white")
+            self.btnDelet.configure(bg="#20292E",fg="white")
+            self.btnChan.configure(bg="#20292E",fg="white")
+            self.btnShow.configure(bg="#20292E",fg="white")
         elif bn == 2:
-            self.btnDelet.configure(bg="#03738C",fg="white")
-            self.btnAdd.configure(bg="#013440",fg="white")
-            self.btnChan.configure(bg="#013440",fg="white")
-            self.btnShow.configure(bg="#013440",fg="white")
+            self.btnDelet.configure(bg="#263238",fg="white")
+            self.btnAdd.configure(bg="#20292E",fg="white")
+            self.btnChan.configure(bg="#20292E",fg="white")
+            self.btnShow.configure(bg="#20292E",fg="white")
         elif bn == 3:
-            self.btnChan.configure(bg="#03738C",fg="white")
-            self.btnDelet.configure(bg="#013440",fg="white")
-            self.btnAdd.configure(bg="#013440",fg="white")
-            self.btnShow.configure(bg="#013440",fg="white")
+            self.btnChan.configure(bg="#263238",fg="white")
+            self.btnDelet.configure(bg="#20292E",fg="white")
+            self.btnAdd.configure(bg="#20292E",fg="white")
+            self.btnShow.configure(bg="#20292E",fg="white")
         elif bn ==4:
-            self.btnAdd.configure(bg="#013440",fg="white")
-            self.btnDelet.configure(bg="#013440",fg="white")
-            self.btnChan.configure(bg="#013440",fg="white")
-            self.btnShow.configure(bg="#03738C",fg="white")
+            self.btnAdd.configure(bg="#20292E",fg="white")
+            self.btnDelet.configure(bg="#20292E",fg="white")
+            self.btnChan.configure(bg="#20292E",fg="white")
+            self.btnShow.configure(bg="#263238",fg="white")
         else:
-            self.btnAdd.configure(bg="#013440",fg="white")
-            self.btnDelet.configure(bg="#013440",fg="white")
-            self.btnChan.configure(bg="#013440",fg="white")
-            self.btnShow.configure(bg="#013440",fg="white")
+            self.btnAdd.configure(bg="#20292E",fg="white")
+            self.btnDelet.configure(bg="#20292E",fg="white")
+            self.btnChan.configure(bg="#20292E",fg="white")
+            self.btnShow.configure(bg="#20292E",fg="white")
 
     def habili(self,bn):
         if bn == 1:
@@ -143,7 +143,7 @@ class Ventana(Frame):
 
     def bSave(self): 
         self.clGrip()
-        if self.cont ==1:
+        if self.cont ==1: #Add
             try:
                 self.Seller.inserta(self.txtID.get(),self.txtName.get(),self.txtValue.get())
                 self.lbl4.config(text="")
@@ -152,7 +152,8 @@ class Ventana(Frame):
                 
             except:
                 self.lbl4.config(text="Valores invalidos.")
-        elif self.cont ==2:
+
+        elif self.cont ==2: #Delet
             try:
                 self.Seller.elimina(self.txtID.get())
                 self.lbl4.config(text="")
@@ -167,7 +168,7 @@ class Ventana(Frame):
             self.Busc()
             
         
-        elif self.cont == 4:
+        elif self.cont == 4: #Change
            try:
                self.lbl4.config(text="")
                self.Val()
@@ -178,47 +179,48 @@ class Ventana(Frame):
           
  
     def create_widgets(self):
-        frame1 = Frame(self, bg="#013440")
-        frame1.place(x=0,y=0,width=100, height=259)        
+        frame1 = Frame(self, bg="#20292E")
+        frame1.place(x=0,y=0,width=100, height=720)        
 
-        self.btnAdd=Button(frame1,text="Añadir", command=self.bAdd, bg="#013440", fg="white",relief=FLAT, )
-        self.btnAdd.place(x=10,y=50,width=80, height=30 )        
-        self.btnDelet=Button(frame1,text="Eliminar", command=self.bDelet, bg="#013440", fg="white",relief=FLAT)
-        self.btnDelet.place(x=10,y=90,width=80, height=30)        
-        self.btnShow=Button(frame1,text="Explorar", command=self.bShow, bg="#013440", fg="white",relief=FLAT)
-        self.btnShow.place(x=10,y=130,width=80, height=30)
-        self.btnChan=Button(frame1,text="Modificar", command=self.bChan, bg="#013440", fg="white",relief=FLAT)
-        self.btnChan.place(x=10,y=200,width=80, height=30)    
+        self.btnAdd=Button(frame1,text="Añadir", command=self.bAdd, bg="#20292E", fg="white",relief=FLAT, )
+        self.btnAdd.place(x=10,y=140,width=80, height=30 )        
+        self.btnDelet=Button(frame1,text="Eliminar", command=self.bDelet, bg="#20292E", fg="white",relief=FLAT)
+        self.btnDelet.place(x=10,y=210,width=80, height=30)        
+        self.btnShow=Button(frame1,text="Explorar", command=self.bShow, bg="#20292E", fg="white",relief=FLAT)
+        self.btnShow.place(x=10,y=280,width=80, height=30)
+        self.btnChan=Button(frame1,text="Modificar", command=self.bChan, bg="#20292E", fg="white",relief=FLAT)
+        self.btnChan.place(x=10,y=650,width=80, height=30)    
 
-        frame2 = Frame(self,bg="#03738C" )
-        frame2.place(x=100,y=0,width=150, height=259)                        
-        lbl1 = Label(frame2,text="ID: ",bg="#03738C",fg="white")        
-        lbl1.place(x=30,y=5)        
+        frame2 = Frame(self,bg="#263238" )
+        frame2.place(x=100,y=0,width=300, height=720)                        
+        lbl1 = Label(frame2,text="ID: ",bg="#263238",fg="white")        
+        lbl1.place(x=30,y=5+100)        
         self.txtID=Entry(frame2)
-        self.txtID.place(x=30,y=25,width=100, height=20)                
-        lbl2 = Label(frame2,text="Nombre: ",bg="#03738C",fg="white")
+        self.txtID.place(x=30,y=25+100,width=160, height=20)                
+        lbl2 = Label(frame2,text="Nombre: ",bg="#263238",fg="white")
 
-        lbl2.place(x=30,y=55)        
+        lbl2.place(x=30,y=55+110)        
         self.txtName=Entry(frame2)
-        self.txtName.place(x=30,y=75,width=100, height=20)        
-        lbl3 = Label(frame2,text="Valor: ",bg="#03738C",fg="white")
-        lbl3.place(x=30,y=105)        
+        self.txtName.place(x=30,y=75+110,width=160, height=20)        
+        lbl3 = Label(frame2,text="Valor: ",bg="#263238",fg="white")
+        lbl3.place(x=30,y=105+120)        
     
-        self.lbl4 = Label(frame2,text="",bg="#03738C",fg="white")
-        self.lbl4.place(x=30,y=210)        
+        self.lbl4 = Label(frame2,text="",bg="#263238",fg="white",font=( NORMAL, 11))
+ 
+        self.lbl4.place(x=35,y=500)        
 
         self.txtValue=Entry(frame2)
-        self.txtValue.place(x=30,y=125,width=100, height=20)        
+        self.txtValue.place(x=30,y=125+120,width=160, height=20)        
           
-        self.btnGuardar=Button(frame2,text="Guardar", command=self.bSave, bg="#039691", fg="white",relief=FLAT)
-        self.btnGuardar.place(x=50,y=160,width=60, height=30)        
+        self.btnGuardar=Button(frame2,text="Guardar", command=self.bSave, bg="#05867B", fg="white",relief=FLAT)
+        self.btnGuardar.place(x=70,y=160+200,width=60, height=30)        
        
 
 
         self.grid = ttk.Treeview(self, columns=("col1","col2","col3"))        
-        self.grid.column("#0",width=50)
+        self.grid.column("#0",width=20)
         self.grid.column("col1",width=60, anchor=CENTER)
-        self.grid.column("col2",width=90, anchor=CENTER)
+        self.grid.column("col2",width=100, anchor=CENTER)
         self.grid.column("col3",width=90, anchor=CENTER)
         
         self.grid.heading("#0", text="#", anchor=CENTER)
@@ -226,7 +228,7 @@ class Ventana(Frame):
         self.grid.heading("col2", text="Nombre", anchor=CENTER)
         self.grid.heading("col3", text="Valor", anchor=CENTER)
         
-        self.grid.place(x=250,y=0,width=420, height=250)
+        self.grid.place(x=310,y=0,width=970, height=720)
         
         
         
